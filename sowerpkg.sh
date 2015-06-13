cb58d99c932a74b9e7629333aabb9a207f787f09
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-06-13 11:47:14 -0300

    Se agrega licencia GPLv3

--- sowerpkg.sh
+++ sowerpkg.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 #
 # SowerPKG
+# Copyright (C) 2015 Esteban De La Fuente Rubio (esteban[at]delaf.cl)
+#
+# Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+# bajo los términos de la Licencia Pública General GNU publicada
+# por la Fundación para el Software Libre, ya sea la versión 3
+# de la Licencia, o (a su elección) cualquier versión posterior de la misma.
+#
+# Este programa se distribuye con la esperanza de que sea útil, pero
+# SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+# MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+# Consulte los detalles de la Licencia Pública General GNU para obtener
+# una información más detallada.
+#
+# Debería haber recibido una copia de la Licencia Pública General GNU
+# junto a este programa.
+# En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+#
+
 #
 # Administrador de paquetes para SowerPHP, permite instalar y actualizar el
 # framework
-#
-# @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-# @version 2015-06-13
 #
 
 # configuración por defecto

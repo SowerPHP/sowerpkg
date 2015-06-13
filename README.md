1b9ec1e93bac7fcdb9052991a9987154aac4c830
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-06-13 02:03:21 -0300

    Se crea sowerpkg.sh para GNU/Linux

---
+++ README.md
@@ -0,0 +1,51 @@
+SowerPKG
+========
+
+SowerPKG corresponde a una herramienta que permite realizar la instalación y actualización del framework SowerPHP y sus extensiones oficiales.
+
+Opciones
+--------
+
+Por defecto SowerPKG utiliza las siguientes opciones:
+
+	EXTENSIONS=""
+	FRAMEWORK_DIR="/usr/share/sowerphp"
+	WWW_DIR="/var/www"
+	COMPOSER="$HOME/bin/composer"
+
+Donde *$HOME* será el directorio principal del usuario con que se ejecuta SowerPKG.
+
+Esta herramienta almacena la configuración del framework en *~/.sowerphp.conf*, esto con el fin de no tener que ingresar las opciones personalizadas cada vez.
+
+Ejemplos de modo de uso
+-----------------------
+
+Instalar con opciones por defecto:
+
+	$ ./sowerpkg.sh install
+
+Instalar framework con extensión empresa y otras requeridas en directorio por defecto más nuevo proyecto en directorio personalizado dentro del *HOME* del usuario:
+
+	$ ./sowerpkg.sh install -e "empresa app general" -w /home/delaf/public_html/webapp
+
+Instalar el framework en directorio personalizado y sin directorio para proyecto web separado del framework:
+
+	$ ./sowerpkg.sh install -d /home/delaf/public_html/sowerphp -W
+
+Consultar ayuda de SowerPKG:
+
+	$ ./sowerpkg.sh --help
+
+**Importante**: siempre utilizar rutas absolutas en los parámetros.
+
+La herramienta solicitará permisos de administrador (con *sudo*) de forma automática si son necesarios. Si el usuario realiza la instalación en directorios donde tiene permisos de escritura el uso de *sudo* no será necesario.
+
+Requerimientos
+--------------
+
+Para realizar la instalación del framework y su posterior uso se requiere como mínimo:
+
+- Servidor web Apache 2.x con mod\_rewrite activo y *AllowOverride All* para el o los directorios que alojarán los proyectos.
+- PHP 5.5 o superior
+- Git y mercurial
+
---
+++ sowerpkg.sh
@@ -0,0 +1,230 @@
+#!/bin/bash
+#
+# SowerPKG
+#
+# Administrador de paquetes para SowerPHP, permite instalar y actualizar el
+# framework
+#
+# @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+# @version 2015-06-13
+#
+
+# configuración por defecto
+EXTENSIONS=""
+FRAMEWORK_DIR="/usr/share/sowerphp"
+WWW_DIR="/var/www"
+COMPOSER="$HOME/bin/composer"
+
+# archivo de configuración
+CONFIG="$HOME/.sowerphp.conf"
+
+# función que guarda las configuraciones en el archivo de configuración
+function sowerphp_config {
+	echo "EXTENSIONS=\"$EXTENSIONS\"" > $CONFIG
+	echo "FRAMEWORK_DIR=\"$FRAMEWORK_DIR\"" >> $CONFIG
+	echo "WWW_DIR=\"$WWW_DIR\"" >> $CONFIG
+	echo "COMPOSER=\"$COMPOSER\"" >> $CONFIG
+}
+
+# función que muestra ayuda del comando
+function sowerphp_help {
+	echo -e "Administrador de paquetes de SowerPHP\n"
+	echo -e "\tModo de uso: $0 ACCIÓN [OPCIONES]\n"
+	echo -e "Acciones:"
+	echo -e "\thelp\t\tMuestra esta ayuda"
+	echo -e "\tinstall\t\tInstala SowerPHP"
+	echo -e "\tupdate\t\tActualiza SowerPHP"
+	echo -e "\nOpciones:"
+	echo -e "\t-e \"...\"\tExtensiones oficiales del framework (separadas por espacio)"
+	echo -e "\t\t\tValor actual: $EXTENSIONS"
+	echo -e "\t-d DIR\t\tDirectorio base del framework"
+	echo -e "\t\t\tValor actual: $FRAMEWORK_DIR"
+	echo -e "\t-w DIR\t\tDirectorio de la aplicación web"
+	echo -e "\t\t\tValor actual: $WWW_DIR"
+	echo -e "\t-W\t\tNo instalar aplicación web (sólo el framework)"
+	echo -e "\t-c COMPOSER\tUbicación de composer"
+	echo -e "\t\t\tValor actual: $COMPOSER"
+	echo -e "\nSi existe $CONFIG se utilizará para sobreescribir las opciones por defecto"
+	echo -e ""
+}
+
+# función que muestra el modo de uso del comando
+function sowerphp_usemode {
+	echo "Modo de uso: $0 ACCIÓN [OPCIONES]"
+	echo "Pruebe '$0 --help' para más información."
+}
+
+# función para verificar requerimientos para la instalación
+function sowerphp_req {
+	CMDS="php git hg"
+	for CMD in $CMDS; do
+		command -v $CMD >/dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			echo "Debe instalar $CMD antes de continuar!"
+			exit 1
+		fi
+	done
+	sowerphp_req_composer
+}
+
+# función para instalar composer si no existe
+function sowerphp_req_composer {
+	command -v $COMPOSER >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		COMPOSER_DIR=`dirname $COMPOSER`
+		COMPOSER_FILE=`basename $COMPOSER`
+		if [ ! -d "$COMPOSER_DIR" ]; then
+			mkdir -p "$COMPOSER_DIR"
+		fi
+		curl -sS https://getcomposer.org/installer | php -- --install-dir=$COMPOSER_DIR --filename=$COMPOSER_FILE > /dev/null
+	fi
+}
+
+# función para realizar la instalación del framework y creación de primer proyecto
+function sowerphp_install {
+	sowerphp_install_framework
+	if [ ! -z "$WWW_DIR" ]; then
+		sowerphp_create_project "$WWW_DIR"
+	fi
+}
+
+# función para realizar la instalación del framework
+function sowerphp_install_framework {
+	# instalar framework
+	echo -n "Instalando SowerPHP... "
+	if [ -d "$FRAMEWORK_DIR" ]; then
+		rm -rf "$FRAMEWORK_DIR" 2> /dev/null
+		if [ $? -ne 0 ]; then
+			sudo rm -rf "$FRAMEWORK_DIR"
+		fi
+	fi
+	mkdir -p "$FRAMEWORK_DIR" 2> /dev/null
+	if [ $? -ne 0 ]; then
+		sudo mkdir -p "$FRAMEWORK_DIR"
+		sudo chown `whoami`: "$FRAMEWORK_DIR" -R
+	fi
+	git clone https://github.com/SowerPHP/sowerphp.git "$FRAMEWORK_DIR" 2> /dev/null
+	if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+	# instalar dependencias de composer
+	cd "$FRAMEWORK_DIR/lib/sowerphp/core"
+	echo -n " Instalando dependencias de composer... "
+	$COMPOSER install 2> /dev/null
+	if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+	# instalar extensiones
+	if [ ! -z "$EXTENSIONS" ]; then
+		echo "Instalando extensiones:"
+		EXTENSIONS_DIR="$FRAMEWORK_DIR/extensions/sowerphp"
+		mkdir -p "$EXTENSIONS_DIR"
+		for EXTENSION in $EXTENSIONS; do
+			echo -n " - Instalando extensión ${EXTENSION}... "
+			git clone https://github.com/SowerPHP/extension-${EXTENSION}.git "$EXTENSIONS_DIR/$EXTENSION" 2> /dev/null
+			if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+			# instalar dependencias de composer
+			cd "$EXTENSIONS_DIR/$EXTENSION"
+			echo -n "    Instalando dependencias de composer... "
+			$COMPOSER install 2> /dev/null
+			if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+		done
+	fi
+}
+
+# función para crear un proyecto
+function sowerphp_create_project {
+	echo -n "Creando projecto web... "
+	WWW_DIR=$1
+	if [ -d "$WWW_DIR" ]; then
+		echo "FAIL!"
+		echo " Directorio $WWW_DIR ya existe!"
+	else
+		PARENT_DIR=`dirname "$WWW_DIR"`
+		if [ ! -d "$PARENT_DIR" ]; then
+			mkdir -p "$PARENT_DIR" 2> /dev/null
+			if [ $? -ne 0 ]; then
+				sudo mkdir -p "$PARENT_DIR"
+				sudo chown `whoami`: "$PARENT_DIR" -R
+			fi
+		fi
+		if [ -d "$FRAMEWORK_DIR/project" ]; then
+			cp -a "$FRAMEWORK_DIR/project" "$WWW_DIR" 2> /dev/null
+			if [ $? -ne 0 ]; then
+				sudo cp -a "$FRAMEWORK_DIR/project" "$WWW_DIR" > /dev/null
+				sudo chown `whoami`: "$WWW_DIR" -R
+			fi
+			sed -i 's/dirname(dirname(dirname(dirname(__FILE__))))/"'"${FRAMEWORK_DIR//\//\\\/}"'"/' "$WWW_DIR/website/webroot/index.php"
+			if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+		else
+			echo "FAIL!"
+			echo " Directorio $FRAMEWORK_DIR/project no existe!"
+		fi
+	fi
+}
+
+# función para realizar la actualización del framework
+function sowerphp_update {
+	# actualizar framework
+	echo -n "Actualizando SowerPHP... "
+	cd "$FRAMEWORK_DIR"
+	git pull origin master >/dev/null 2>&1
+	if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+	# actualizar dependencias de composer
+	cd "$FRAMEWORK_DIR/lib/sowerphp/core"
+	echo -n " Actualizando dependencias de composer... "
+	$COMPOSER update 2> /dev/null
+	if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+	# actualizar extensiones
+	if [ ! -z "$EXTENSIONS" ]; then
+		EXTENSIONS_DIR="$FRAMEWORK_DIR/extensions/sowerphp"
+		for EXTENSION in $EXTENSIONS; do
+			echo -n " - Actualizando extensión ${EXTENSION}... "
+			cd "$EXTENSIONS_DIR/$EXTENSION"
+			git pull origin master >/dev/null 2>&1
+			if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+			# actualizar dependencias de composer
+			echo -n "    Actualizando dependencias de composer... "
+			$COMPOSER update 2> /dev/null
+			if [ $? -eq 0 ]; then echo "OK!"; else echo "FAIL!"; fi
+		done
+	fi
+}
+
+# si no se pasó ninguna acción se muestra mensaje modo de uso
+if [ $# -eq 0 ]; then
+	sowerphp_usemode
+	exit 1
+fi
+
+# obtener acción
+ACCION=$1
+shift
+
+# cargar archivo de configuración si existe
+if [ -r "$CONFIG" ]; then
+	. $CONFIG
+fi
+
+# parsear argumentos
+while [[ $# > 0 ]]; do
+	case $1 in
+		-e) EXTENSIONS="$2"; shift;;
+		-d) FRAMEWORK_DIR="$2"; shift;;
+		-w) WWW_DIR="$2"; shift;;
+		-W) WWW_DIR="";;
+		-c) COMPOSER="$2"; shift;;
+	esac
+	shift
+done
+
+# guardar configuraciones en archivo de configuración
+sowerphp_config
+
+# verificar requerimientos para instalación
+sowerphp_req
+
+# ejecutar acción
+case $ACCION in
+	install) sowerphp_install;;
+	update) sowerphp_update;;
+	help|--help|-h) sowerphp_help;;
+	*) sowerphp_usemode; exit 1;;
+esac
+

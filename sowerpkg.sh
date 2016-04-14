237d12b08208b61fcf26c4c2b47bbaa1c346c79a
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-04-14 01:28:53 -0300

    De GPL a AGPL

--- sowerpkg.sh
+++ sowerpkg.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 #
 # SowerPKG
-# Copyright (C) 2015 Esteban De La Fuente Rubio (esteban[at]delaf.cl)
+# Copyright (C) SowerPHP (http://sowerphp.org)
 #
-# Este programa es software libre: usted puede redistribuirlo y/o modificarlo
-# bajo los términos de la Licencia Pública General GNU publicada
-# por la Fundación para el Software Libre, ya sea la versión 3
-# de la Licencia, o (a su elección) cualquier versión posterior de la misma.
+# Este programa es software libre: usted puede redistribuirlo y/o
+# modificarlo bajo los términos de la Licencia Pública General Affero de GNU
+# publicada por la Fundación para el Software Libre, ya sea la versión
+# 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+# misma.
 #
 # Este programa se distribuye con la esperanza de que sea útil, pero
 # SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
 # MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
-# Consulte los detalles de la Licencia Pública General GNU para obtener
-# una información más detallada.
+# Consulte los detalles de la Licencia Pública General Affero de GNU para
+# obtener una información más detallada.
 #
-# Debería haber recibido una copia de la Licencia Pública General GNU
+# Debería haber recibido una copia de la Licencia Pública General Affero de GNU
 # junto a este programa.
-# En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+# En caso contrario, consulte <http://www.gnu.org/licenses/agpl.html>.
 #
 
 #
@@ -247,4 +248,3 @@ case $ACCION in
 	help|--help|-h) sowerphp_help;;
 	*) sowerphp_usemode; exit 1;;
 esac
-
